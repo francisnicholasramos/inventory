c554d3d06c75e2084613d3988bb6c24c5fe72a92
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-10-13 01:50:43 +0800

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import path from "path";
+import {router} from "./routes/indexRoutes"
 
 const app = express();
 
 const port = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/styles", express.static(path.join(__dirname, "styles")));
+
+app.use("/", router)
+
 app.listen(port, () => {
     console.log(`Service running on http://localhost:${port}`);
 });
+
