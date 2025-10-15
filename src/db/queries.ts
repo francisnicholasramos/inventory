ed399fe6007d3c6007a1329d2292294b075f9274
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-10-16 04:02:59 +0800

    Remove console logs

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,9 +1,7 @@
 import {pool} from './pool';
 
 export async function getItems() {
-    const {rows} = await pool.query("SELECT * FROM usernames")
-    console.log(rows)
-    return rows;
+    return await pool.query("SELECT * FROM items");
 }
 
 export async function insertItem(params: string) {
@@ -13,24 +11,20 @@ export async function insertItem(params: string) {
 
 export async function searchItem(params: string) {
     const {rows} = await pool.query("SELECT username FROM usernames WHERE username ILIKE $1", [`%${params}%`])
-    console.log(rows);
     return rows;
 }
 
 export async function deleteItem(id: number) {
     const {rows} = await pool.query("DELETE FROM usernames WHERE id=$1", [id])
-    console.log(rows);
     return rows;
 }
 
 export async function updateItem(id: number, params: string) {
     const {rows} = await pool.query("UPDATE usernames SET username=$2 WHERE id=$1", [id, params])
-    console.log(rows)
     return rows;
 }
 
 export async function getItemId(id: number) {
     const {rows} = await pool.query("SELECT username FROM usernames WHERE id=$1", [id])
-    console.log(rows);
     return rows;
 }
