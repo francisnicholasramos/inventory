79844c2376b349e0013d04b183545739fa449eb9
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-10-28 06:29:20 +0800

    Update

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../src/views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "../public")));
