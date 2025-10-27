8b1479004294f5c1bdea2d2875578193dd3bbb31
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-10-28 06:39:50 +0800

    fix: correct scripts and style paths

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,8 @@ app.set("views", path.join(__dirname, "../src/views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "scripts")));
-app.use("/styles", express.static(path.join(__dirname, "styles")));
+app.use(express.static(path.join(__dirname, "../src/scripts")));
+app.use("/styles", express.static(path.join(__dirname, "../src/styles")));
 
 app.use("/", category)
 app.use("/", router)
