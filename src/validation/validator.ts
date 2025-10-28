3eaab6671d30de00e365b5dbe9463183a24e0fdf
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-10-29 05:17:42 +0800

    Fix handling

--- src/validation/validator.ts
+++ src/validation/validator.ts
@@ -1,5 +1,7 @@
 import {checkSchema} from "express-validator";
 
+// properties and input "name" attribute should be the same
+
 export const validateInput = checkSchema({
   item_name: {
     in: ["body"],
@@ -10,6 +12,10 @@ export const validateInput = checkSchema({
     }, 
     notEmpty: {
       errorMessage: "Item name is required."
+    },
+  custom: {
+      options: (value) => isNaN(Number(value)),
+      errorMessage: "Cannot be a number."
     }
   },
 
@@ -20,17 +26,22 @@ export const validateInput = checkSchema({
       options: {max: 255},
       errorMessage: "Description for the item should not exceed by 255 long."
     },
-    notEmpty: { errorMessage: "Description cannot be empty."}
+    notEmpty: { errorMessage: "Description cannot be empty."},
+    custom: {
+        options: (value) => isNaN(Number(value)),
+        errorMessage: "Cannot be a number."
+    }
+
   },
 
   price: {
     in: ["body"],
+    notEmpty: { errorMessage: "Price is required."},
     isNumeric: { errorMessage: "Price must be a number."},
     isLength: {
       options: {max: 5},
       errorMessage: "Max of 5 digits only."
     },
-    notEmpty: { errorMessage: "Price is required."}
   },
 
   category: {
@@ -41,5 +52,17 @@ export const validateInput = checkSchema({
       errorMessage: "Are you nuts? 35 characters long is a form of autism."
     },
     notEmpty: {errorMessage: "Items should have a category."}
-  }
+  },
 });
+
+export const categoryInputValidate = checkSchema({ 
+    category_name: {
+        in: ["body"],
+        isString: {errorMessage: "Category cannot be a number."},
+        notEmpty: {errorMessage: "Input cannot be empty."},
+        custom: {
+            options: (value) => isNaN(Number(value)),
+            errorMessage: "Cannot be a number."
+        }
+    }
+}) 
